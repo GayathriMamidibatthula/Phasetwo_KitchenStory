1fa20c5bb9d44fe15a29c598619c15e562f564a4
GayathriMamidibatthula <gayathrimamidibatthula@gmail.com>
2021-05-07 11:56:10 +0530

    Initial commit

---
+++ js/order.js
@@ -0,0 +1,8 @@
+function order(){
+    alert("Thank you for shopping with Kitchen Story! Your order has been placed!")
+    
+  }
+
+  const login=()=>{
+window.location='loginpage.html'
+  }
---
+++ js/products.js
@@ -0,0 +1,50 @@
+products = {
+    123: {
+      name : "Aashirvaad Atta (5 kg)",
+      desc : "Aashirvaad Atta with Multigrains provides you and your family wholesome nutrition. India’s No. 1 Atta brand, is made with love, with the grains sourced from the fields across the country ",
+      img : "aata.jpg",
+      price : 239
+    },
+    124: {
+      name : "Quaker Oats Pouch (1000 g)",
+      desc : "Quaker oats is made from 100 % wholegrain oats, which is a natural source of carbohydrates, protein and dietary fibre. Top it up with your favourite fruits and nuts to make it more delicious ",
+      img : "oats.jpg",
+      price : 171
+    },
+    125: {
+      name : "Sun Shudhi Refined Sunflower Oil  (1 L)",
+      desc : "Refined, imported and rich in Omega 3 & 6 fatty acids and Vit E",
+      img : "oil.jpg",
+      price : 675
+    },
+    126: {
+      name : "Fresh Brocolli (500g)",
+      desc : "Fresh, hygienic and natural. Good source of Vitamin A, B6, C, and Magnesium. Store in cool and dry place, away from direct sunlight. Can be used in salads or soups ",
+      img : "brocolli.jpg",
+      price : 81
+    },
+    127: {
+      name : "Fresh Pomegranate, 4 Pieces",
+      desc : "Fresh, hygienic and natural. Rich in Vitamin C, K , folate and Potassium. Store in cool and dry place, away from direct sunlight. Pack contains 4 peices of pomegranate. No added preservatives ",
+      img : "pomegranate.jpg",
+      price : 175
+    },
+    128: {
+      name : "Banana (1 kg)",
+      desc : "Fresh and hygienic bananas with no added preservatives",
+      img : "banana.jpg",
+      price : 10
+    },
+    129: {
+      name : "Fresh Sweet Lime (1 kg)",
+      desc : "100% natural, fresh and rich in taste, directly imported from Gujarat",
+      img : "sweetlime.jpg",
+      price : 15
+    },
+    130: {
+      name : "Fresh Shimla Apple(1 KG) ",
+      desc : "Fresh baby apples directly imported from Shimla. Good source of Vitamin c, dietary fiber, flavonoids and antioxidants ",
+      img : "apple.jpg",
+      price : 100
+    }
+  };
